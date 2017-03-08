2ab776ff03ef2d094aa01e90675de103ecae6c9e
imapepper <joecool520@gmail.com>
2017-03-08 00:41:56 -0600

    Improved CheckerPiece image size and colors of checkerboard.

--- src/main/java/edu/iastate/mis307/Main.java
+++ src/main/java/edu/iastate/mis307/Main.java
@@ -9,8 +9,8 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        ImageIcon blackPiece = new ImageIcon(ClassLoader.getSystemResource("img/blackCheckerPiece50x50.png"));
-        ImageIcon redPiece = new ImageIcon(ClassLoader.getSystemResource("img/redCheckerPiece50x50.png"));
+        ImageIcon blackPiece = new ImageIcon(ClassLoader.getSystemResource("img/blackCheckerPiece80x80.png"));
+        ImageIcon redPiece = new ImageIcon(ClassLoader.getSystemResource("img/redCheckerPiece80x80.png"));
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(8, 8));
@@ -21,20 +21,30 @@ public class Main {
             for (int j = 0; j < 8; j++) {
                 Location location = locations[i][j];
                 CheckerPiece piece;
+                JLabel label = new JLabel();
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                label.setPreferredSize(new Dimension(50, 50));
+
+                if(location.isPlayable()) {
+                    label.setBackground(new Color(78, 49, 36));
+                } else {
+                    label.setBackground(new Color(212, 164, 114));
+                }
                 if((piece = location.getPiece()) != null) {
                     if("black".equals(piece.getColor())) {
-                        panel.add(new JLabel(blackPiece));
+                        label.setIcon(blackPiece);
                     } else {
-                        panel.add(new JLabel(redPiece));
+                        label.setIcon(redPiece);
                     }
-                } else {
-                    panel.add(new JLabel("-----X-----"));
                 }
+
+                label.setOpaque(true);
+                label.setHorizontalAlignment(JLabel.CENTER);
+                panel.add(label);
             }
         }
-
         frame.add(panel);
-        frame.setSize(1200, 800);
+        frame.setSize(800, 800);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
