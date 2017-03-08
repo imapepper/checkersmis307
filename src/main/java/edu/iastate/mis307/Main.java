7d475df50ec57aab86c29b5c819fd355c0a844e0
imapepper <joecool520@gmail.com>
2017-03-08 02:03:36 -0600

    Changed layout to GridBagLayout for better component management. Fixed image for black checker piece.

--- src/main/java/edu/iastate/mis307/Main.java
+++ src/main/java/edu/iastate/mis307/Main.java
@@ -6,21 +6,28 @@ import edu.iastate.mis307.objects.Location;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        ImageIcon blackPiece = new ImageIcon(ClassLoader.getSystemResource("img/blackCheckerPiece80x80.png"));
-        ImageIcon redPiece = new ImageIcon(ClassLoader.getSystemResource("img/redCheckerPiece80x80.png"));
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(8, 8));
+        Vector<JLabel> jLabels = new Vector<>(64, 64);
+        panel.setLayout(new GridBagLayout());
+
+        ImageIcon blackPiece = new ImageIcon(ClassLoader.getSystemResource("img/blackCheckerPiece80x80.png"));
+        ImageIcon redPiece = new ImageIcon(ClassLoader.getSystemResource("img/redCheckerPiece80x80.png"));
 
         Board board = new Board();
         Location[][] locations = board.getSpaces();
+
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 Location location = locations[i][j];
                 CheckerPiece piece;
+                GridBagConstraints constraints = new GridBagConstraints(j, i, 1, 1,
+                        0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                        new Insets(0, 0, 0, 0), 40, 40);
                 JLabel label = new JLabel();
                 label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
                 label.setPreferredSize(new Dimension(50, 50));
@@ -37,10 +44,10 @@ public class Main {
                         label.setIcon(redPiece);
                     }
                 }
-
                 label.setOpaque(true);
                 label.setHorizontalAlignment(JLabel.CENTER);
-                panel.add(label);
+                panel.add(label, constraints);
+                jLabels.addElement(label);
             }
         }
         frame.add(panel);
